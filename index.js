b46e53e20e2e737425cb729bc5d5274b3375f4f1
Anantha Debnath <ananthadebnath103@gmail.com>
2023-05-27 01:38:26 +0600

    tring to ignore the node modules

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+// const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+// app.use(cors());
+
+app.get('/', (req, res) =>{
+    res.send('This is my first ever server')
+});
+
+app.listen(port, () => {
+    console.log(`Server API is running on port: ${port}`)
+})
