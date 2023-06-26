f78b6827a2f70d06d9539a5388f62b5c281f8191
Anantha Debnath <ananthadebnath103@gmail.com>
2023-06-26 21:16:49 +0600

    data loded for chef

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 
-
+const chefs = require('./Data/Chefs.json')
 
 app.get('/', (req, res) => {
     res.send('chef running')
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
 });
 
 
